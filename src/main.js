51a7c90f6cd95d0bebfe3b54f212797fef1fc060
Tim <drtimde@hotmail.com>
2020-09-30 16:22:06 +0200

    All great things begin with something small

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// reverse proxy front-end request to http://localhost:8443/api
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+Vue.prototype.$axios = axios
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
